65d002e212382800bfef5388011a4fbee0b34693
a5ea5em5int <ayeayemyint1073@gmail.com>
2022-10-29 10:51:48 +0630

    First project setup using spring web and thymeleaf dependencies

---
+++ week2day1/src/main/java/com/software/week2/Week2day1Application.java
@@ -0,0 +1,13 @@
+package com.software.week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2day1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2day1Application.class, args);
+	}
+
+}
